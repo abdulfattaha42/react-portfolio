e2088358f12951bb0c15cfbb128fae234d5a139c
Abdulfattah Ahmed <abdulfattahahmed@Abdulfattahs-MacBook-Pro.local>
2025-10-03 22:28:33 -0400

    Fix NavBar import issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navbar } from './components/NavBar';
+import { Navbar } from './components/Navbar';
 import { Hero } from './components/Hero';
 import About from './components/about';
 import Technologies from './components/Technologies';
@@ -12,7 +12,8 @@ const App = () => {
     <div className="overflow-x-hidden text-neutral-300 antialiased
     selection:bg-cyan-300 selection:text-cyan-900">
       <div className="fixed top-0 -z-10 h-full w-full">
-        <div class="relative h-full w-full bg-slate-950"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#3e3e3e,transparent)]"></div></div>
+        <div class="relative h-full w-full bg-slate-950">
+          <div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#3e3e3e,transparent)]"></div></div>
       </div>
       <div className="container mx-auto px-8">
       <Navbar />
