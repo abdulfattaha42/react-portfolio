5dc5020acb83ea43a96e0ca125cd3e4ea1594960
Abdulfattah Ahmed <abdulfattahahmed@macbookpro.myfiosgateway.com>
2025-10-22 02:09:18 -0400

    Updated hero image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { HERO_CONTENT } from '../constants'
-import profilePic from "../assets/suit1.jpg"
+import profilePic from "../assets/headshot.png"
 import { motion } from 'framer-motion'
 
 const container = (delay) => ({
@@ -32,7 +32,7 @@ export const Hero = () => {
                         animate="visible" 
                         className="bg-gradient-to-r from-pink-300
                         via-slate-500 to-purple-500 bg-clip-text text-3xl tacking-tight text-transparent">
-                        User Experience Engineer
+                        Software Engineer
                         </motion.span>
                         <motion.p 
                         variants={container(1)}
